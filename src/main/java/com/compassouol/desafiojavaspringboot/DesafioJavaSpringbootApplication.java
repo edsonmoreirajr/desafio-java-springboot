e36d4b3c51676898a6c60f70a235383ef46dde12
edsonmoreirajr <edsonmoreirajr@gmail.com>
2021-03-20 13:11:36 -0300

    Primeiro commit. Inicialização do projeto

---
+++ src/main/java/com/compassouol/desafiojavaspringboot/DesafioJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.compassouol.desafiojavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioJavaSpringbootApplication.class, args);
+	}
+
+}
