33065752243b26c467246d10efd33ce4d960dac4
Edson Junior <edsonmoreirajr@gmail.com>
2021-03-20 21:22:56 -0300

    Criação classe model de Product

---
+++ src/main/java/com/compassouol/productms/domain/model/Product.java
@@ -0,0 +1,34 @@
+package com.compassouol.productms.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Product {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_product", unique=true, nullable=false)
+	private Long idProduct;
+	
+	@Column(name="name", nullable=false, length=80)
+	private String name;
+	
+	@Column(name="description", nullable=false, length=150)
+	private String description;
+	
+	@Column(name="price", nullable=false, precision=10, scale=2)
+	private BigDecimal price;
+	
+}
