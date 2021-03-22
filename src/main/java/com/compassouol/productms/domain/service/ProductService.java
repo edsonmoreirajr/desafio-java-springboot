755483ddc54090dda15209079c5bf66f49c351aa
Edson Junior <edsonmoreirajr@gmail.com>
2021-03-22 10:03:22 -0300

    Refatoração da classe ProductService, remoção de import

--- src/main/java/com/compassouol/productms/domain/service/ProductService.java
+++ src/main/java/com/compassouol/productms/domain/service/ProductService.java
@@ -9,7 +9,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.compassouol.productms.core.data.PageableTranslator;
 import com.compassouol.productms.domain.exception.not_found.ProductNaoEncontradoException;
